37c4f6c2432c2b45c3d61188109908696908d39f
osistlk <osistlk@gmail.com>
2024-03-09 11:14:55 -0500

    Update main.js and preload.js

--- preload.js
+++ preload.js
@@ -4,19 +4,6 @@ window.myAPI = {
     desktop: true
 }
 
-contextBridge.exposeInMainWorld('systemStats', {
-    getUsage: () => ipcRenderer.invoke('cpu-usage'),
-    getMemory: () => ipcRenderer.invoke('memory-usage'),
-    getDisk: () => ipcRenderer.invoke('disk-usage'),
-    getNetwork: () => ipcRenderer.invoke('network-usage'),
-    getBattery: () => ipcRenderer.invoke('battery-usage'),
-    getTemperature: () => ipcRenderer.invoke('temperature-usage'),
-    getGPU: () => ipcRenderer.invoke('gpu-usage'),
-    getProcesses: () => ipcRenderer.invoke('processes-usage'),
-    getFileSystem: () => ipcRenderer.invoke('file-system-usage'),
-    getOS: () => ipcRenderer.invoke('os-usage'),
-    getSystem: () => ipcRenderer.invoke('system-usage'),
-    getDisplay: () => ipcRenderer.invoke('display-usage'),
-    getSound: () => ipcRenderer.invoke('sound-usage'),
-    getUSB: () => ipcRenderer.invoke('usb-usage'),
+contextBridge.exposeInMainWorld('mainAPI', {
+    test: () => ipcRenderer.invoke('test'),
 })
--- renderer.js
+++ renderer.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('waveCanvas');
 const ctx = canvas.getContext('2d');
 
 let time = 0;
-let waveAmplitude = 0.000001;
+let waveAmplitude = 10;
 const waveFrequency = 0.01;
 
 function draw() {
@@ -26,9 +26,6 @@ function draw() {
 
 document.addEventListener('DOMContentLoaded', async () => {
     console.log('DOM content loaded')
-    const cpuUsage = await window.systemStats.getUsage() + '';
-    console.log(cpuUsage);
-    waveAmplitude *= cpuUsage;
 });
 
 
