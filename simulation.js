a3b534a3efcaec0eacd35df5ab0efcc990e4f8ca
osistlk <osistlk@gmail.com>
2024-03-15 20:09:26 -0400

    Refactor Particle class to include speed and direction

--- simulation.js
+++ simulation.js
@@ -4,47 +4,40 @@ const ProgressBar = require('progress');
 const path = require('path');
 
 class Particle {
-    constructor(x, y, mass) {
+    constructor(x, y, speed, direction) {
         this.x = x;
         this.y = y;
-        this.mass = mass;
-        this.vx = 0;
-        this.vy = 0;
+        this.speed = speed;
+        this.direction = direction;
+    }
+
+    update(elapsedTime) {
+        // Change the position based on the speed, direction and elapsed time
+        this.x += this.speed * Math.cos(this.direction) * elapsedTime;
+        this.y += this.speed * Math.sin(this.direction) * elapsedTime;
     }
 
     kineticEnergy() {
         let v = Math.sqrt(this.vx * this.vx + this.vy * this.vy);
         return 0.5 * this.mass * v * v;
     }
-    update() {
-        this.x += this.vx;
-        this.y += this.vy;
-    }
 }
 
 
 class Simulation {
+
     constructor() {
         this.particles = [];
-        this.centralMass = 10000;
-        this.G = 6.674 * Math.pow(10, -11); // gravitational constant
     }
 
     addParticle(particle) {
         this.particles.push(particle);
     }
 
-    update() {
+    update(elapsedTime) {
+        // Update each particle
         for (let particle of this.particles) {
-            let dx = 0 - particle.x;
-            let dy = 0 - particle.y;
-            let distance = Math.sqrt(dx * dx + dy * dy);
-            let force = this.G * this.centralMass * particle.mass / (distance * distance);
-            let forceX = force * dx / distance;
-            let forceY = force * dy / distance;
-            particle.vx += forceX / particle.mass;
-            particle.vy += forceY / particle.mass;
-            particle.update();
+            particle.update(elapsedTime);
         }
     }
 
